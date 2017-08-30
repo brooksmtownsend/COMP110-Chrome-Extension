0acc1f477e5ffdd1b4390c737f1b0c6af6b71e05
Brooks Townsend <brooksmtownsend@gmail.com>
2017-08-30 08:32:05 -0400

    Removed crx extension from workspace, updated code to improve notification functionality

--- background.js
+++ background.js
@@ -1,3 +1,4 @@
+//When a user clicks on the icon, will execute content.js
 chrome.browserAction.onClicked.addListener(function(tab) {
    chrome.tabs.executeScript(null, {file: "content.js"});
 });
--- content.js
+++ content.js
@@ -1,21 +1,39 @@
 Notification.requestPermission().then(function(result) {
-  console.log(result);
-  window.alert("Notifications Enabled!");
+    console.log("permission " + result);
 });
 
 var queue = document.querySelector('.queue')
 
-queue.addEventListener('DOMNodeInserted', function(event) {
-    var target = event.target;
-    // Checking if the target element is a new ticket (not just called)
-    if (!target.querySelector('img')) {
-        console.log(target);
-        var title = 'A new student has arrived';
-        var noti = new Notification(
-            title, {
-                body: target.querySelector('.problem').textContent,
-                icon: 'http://comp110.com/cpresources/userphotos/kris@cs.unc.edu/100/Screen-Shot-2015-12-27-at-11.06.24-AM.png?d=1451835909'
+try{
+    queue.addEventListener('DOMNodeInserted', function(event) {
+        var target = event.target;
+        // Checking if the target element is a new ticket (not just called)
+        if (!target.querySelector('img')) {
+            console.log(target);
+            var title = 'A new student has arrived';
+            var noti = new Notification(
+                title, {
+                    body: target.querySelector('.problem').textContent,
+                    icon: 'http://comp110.com/cpresources/userphotos/kris@cs.unc.edu/100/Screen-Shot-2015-12-27-at-11.06.24-AM.png?d=1451835909'
+                }
+            );
+            noti.onclick = function(event) {
+                window.focus();
             }
-        );
+        }
+    });
+    var noti = new Notification("COMP110 Notifications Started!", null);
+    noti.onclick = function(event) {
+        window.focus();
+        
     }
-});
+} catch (TypeError){
+    console.log("The queue was not found on the page. Make sure you are on the office hours staff help page and you are checked in to the queue.")
+    var title = "Error starting Office Hours Notifications";
+    var noti = new Notification(
+        title, {
+            body: "The queue was not found on the page. Make sure you are on the office hours staff help page and you are checked in to the queue.",
+            icon: 'http://comp110.com/cpresources/userphotos/kris@cs.unc.edu/100/Screen-Shot-2015-12-27-at-11.06.24-AM.png?d=1451835909'
+        }
+    );
+}
